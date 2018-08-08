8eceacb41f7d84d103ca0709cf62f145b464a40b
Daisuke Tanaka <duaipp@gmail.com>
2018-08-08 16:44:03 +0900

    add module information as setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name="travis-deploy",
+    version='0.0.1',
+    url='https://github.com/disktnk/travis-deploy',
+    author='disktnk',
+    author_email='duaipp@gmail.com',
+    description='Travis deploy test',
+    long_description='Test automatically deploy via Travis CI',
+    packages=['tools'],
+    install_requires='',
+    license="MIT License"
+)
