1fb89bf5a052e3087fa1280fe93a003dcd769579
Daisuke Tanaka <duaipp@gmail.com>
2018-08-08 17:34:45 +0900

    support python2

---
+++ tools/msg.py
@@ -0,0 +1,5 @@
+import tools._version
+
+
+def msg():
+    print('hello, %s' % tools._version.__version__)
--- tools/tools.py
+++
@@ -1,5 +0,0 @@
-from tools import _version as v
-
-
-def msg():
-    print('hello, %s' % v.__version__)
