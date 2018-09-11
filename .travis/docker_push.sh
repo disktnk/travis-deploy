9338516f41d0765a0839c6f69540e50e8110b184
Daisuke Tanaka <duaipp@gmail.com>
2018-09-11 16:57:17 +0900

    replace getting git tag

--- .travis/docker_push.sh
+++ .travis/docker_push.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-export TRAVIS_DEPLOY_LATEST_VER=`git describe --abbrev=0 --tags`
-echo "$TRAVIS_DEPLOY_LATEST_VER"
+echo "$TRAVIS_TAG"
 echo "$DOCKERHUB_MAINTAINER_PASS" | docker login -u "$DOCKERHUB_MAINTAINER_NAME" --password-stdin
-docker build -t disktnk/travis-deploy:"$TRAVIS_DEPLOY_LATEST_VER" -f docker/Dockerfile .
-docker push disktnk/travis-deploy:"$TRAVIS_DEPLOY_LATEST_VER"
+docker build -t disktnk/travis-deploy:"$TRAVIS_TAG" -f docker/Dockerfile .
+docker push disktnk/travis-deploy:"$TRAVIS_TAG"
