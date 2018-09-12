b143fe6d6bc1e477c9408093f9ebea32eba36466
Daisuke Tanaka <duaipp@gmail.com>
2018-09-12 11:05:45 +0900

    remove own build

--- .travis/docker_push.sh
+++ .travis/docker_push.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "$TRAVIS_TAG"
-python setup.py sdist
 echo "$DOCKERHUB_MAINTAINER_PASS" | docker login -u "$DOCKERHUB_MAINTAINER_NAME" --password-stdin
 docker build -t disktnk/travis-deploy:"$TRAVIS_TAG" -f docker/dockerhub/Dockerfile --build-arg TRAVIS_DEPLOY_VERSION="${TRAVIS_TAG:1}" .
 docker push disktnk/travis-deploy:"$TRAVIS_TAG"
